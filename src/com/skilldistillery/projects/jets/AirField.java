8790354a59d8706f9b5122e13f53ec47e3815353
Xian Zheng <xzheng2112@gmail.com>
2018-03-10 11:07:55 -0700

    Jets array and 5 jets created inside AirField

--- src/com/skilldistillery/projects/jets/AirField.java
+++ src/com/skilldistillery/projects/jets/AirField.java
@@ -3,5 +3,21 @@ package com.skilldistillery.projects.jets;
 public class AirField {
 	
 	private Jet [] jets;
+	
+	public AirField() {
+		Jet cargo1 = new CargoPlane("Boeing 777 Freighter", 901.21 , 9065, 295_700_000);
+		Jet cargo2 = new CargoPlane("Boeing Dreamlifter", 878.00, 7800, 232_000_000);
+		Jet fighter1 = new FighterJet("A-10 Warthog", 833.62, 4148, 20_000_000);
+		Jet fighter2 = new FighterJet("F 15E Strike Eagle", 2655.35, 3889, 36_000_000);
+		Jet plain1 = new PlainJet("Boeing BBJ", 827.18, 7223, 65_000_000);
+		
+		Jet [] jets = new Jet[100];
+		jets[0] = cargo1;
+		jets[1] = cargo2;
+		jets[2] = fighter1;
+		jets[3] = fighter2;
+		jets[4] = plain1;
+		
+	}
 
 }
--- src/com/skilldistillery/projects/jets/JetApplication.java
+++ src/com/skilldistillery/projects/jets/JetApplication.java
@@ -2,12 +2,12 @@ package com.skilldistillery.projects.jets;
 
 import java.util.Scanner;
 
+
 public class JetApplication {
-//	private Airfield airfield;
+	AirField airfield = new AirField();
 	private static Scanner kb = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
-		
 
 	}
 
